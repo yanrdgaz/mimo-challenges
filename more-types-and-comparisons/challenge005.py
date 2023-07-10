91147b7a85496cb233c8740ce658b9264abd6f7e
Yan Rodrigues <yanrdgaz@gmail.com>
2023-07-10 15:53:34 -0300

    Challenge 005 completed.

---
+++ more-types-and-comparisons/challenge005.py
@@ -0,0 +1,7 @@
+# Jonas is going to the movies. He checked the prices and he'll need a ticket for one adult.
+# Tasks (1): Complete the f-string to show that he'll buy a ticket for an adult by using the correct variable.
+
+age = 15
+adult_ticket = age >= 12
+
+print(f"Buy one adult ticket: {adult_ticket}")
