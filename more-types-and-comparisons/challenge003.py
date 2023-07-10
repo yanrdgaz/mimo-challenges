f822c8974ec2da8383fe5fc19d432c4c8531e056
Yan Rodrigues <yanrdgaz@gmail.com>
2023-07-10 15:03:59 -0300

    Challenge 003 completed.

---
+++ more-types-and-comparisons/challenge003.py
@@ -0,0 +1,11 @@
+# Sam is 16 years old and wants to know if she's old enough to drive a car. Complete the missing operators to show
+# that anyone younger than 17 is not allowed to drive. Tasks (2): Finish the too_young comparison to answer the
+# question in the console with a Boolean that says that Sam's too young to drive. Finish the car_driver comparison to
+# show that she cannot drive a car.
+
+sams_age = 16
+too_young = sams_age < 17
+car_driver = sams_age > 17
+
+print(f"Is Sam too young to drive? {too_young}")
+print(f"Can Sam drive a car? {car_driver}")
