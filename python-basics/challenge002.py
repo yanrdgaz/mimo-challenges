3a41ad8b1074f09efa9b441a3a82358c544376d7
Yan Rodrigues <yanrdgaz@gmail.com>
2023-07-10 14:49:44 -0300

    Challenge 002 completed.

---
+++ python-basics/challenge002.py
@@ -0,0 +1,13 @@
+# Your friend has a youtube channel. They asked you to figure out how well their channel is doing. Let's find their best and worst videos!
+# Tasks (3):
+# Create a mostViewed variable, and assign the value "7 Amazing Mugs I CANNOT live without" to it.
+# Create a leastViewed variable, and assign the value "The Best Mug to Take Scuba Diving" to it.
+# Use two print statements to output these variables in the console, each on a different line. Make sure the output is the following:
+# 7 Amazing Mugs I CANNOT live without
+# The Best Mug to Take Scuba Diving
+
+mostViewed = "7 Amazing Mugs I CANNOT live without"
+leastViewed = "The Best Mug to Take Scuba Diving"
+
+print(mostViewed)
+print(leastViewed)
