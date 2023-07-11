2eac026edcdead662f3574c534851631e56f65c6
Yan Rodrigues <yanrdgaz@gmail.com>
2023-07-11 17:31:45 -0300

    School Grades Program completed.

---
+++ more-types-and-comparisons/schoolGradesProgram.py
@@ -0,0 +1,30 @@
+# 1.
+student_name = "Sam"
+
+# 2.
+math_grade = 88
+science_grade = 75
+english_grade = 90
+geology_grade = 69
+
+# 3.
+total_grade = math_grade + science_grade + english_grade + geology_grade
+
+# 4.
+max_grade = 400
+total_percentage = (total_grade/max_grade)*100
+print(f"Sam's total percentage is {total_percentage}%")
+
+# 5.
+total_percentage = int(total_percentage)
+
+# 6.
+did_student_pass = total_percentage >= 50
+
+# 7.
+sporting_activities = bool(0)
+print(f"Sam participated in sporting activities: {sporting_activities}")
+
+# 8.
+print(f"Sam's total percentage as an integer is {total_percentage}%")
+print(f"Sam passed to the next semester: {did_student_pass}")
