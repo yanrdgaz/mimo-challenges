71ec8f4bf4441119281aa0e67dfee25ed12ae682
Yan Rodrigues <yanrdgaz@gmail.com>
2023-07-24 10:20:05 -0300

    Challenge001.py changed.

--- python-basics/challenge001.py
+++ python-basics/challenge001.py
@@ -1,9 +1,7 @@
-# Your favourite band uploaded a new video! Let's see how many people watched and commented on it.
-# Tasks (4):
-# Create a views variable and assign the value 1145 to it.
-# Create a comments variable and assign the value 200 to it.
-# Use a print statement to output the views variable to the console.
-# Use a print statement to output the comments variable to the console.
+# Your favourite band uploaded a new video! Let's see how many people watched and commented on it. Tasks (4): Create
+# a views variable and assign the value 1145 to it. Create a comments variable and assign the value 200 to it. Use a
+# print statement to output the views variable to the console. Use a print statement to output the comments variable
+# to the console.
 
 views = 1145
 comments = 200
